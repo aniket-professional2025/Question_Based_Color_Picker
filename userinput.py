bef915a4a6f6e26694b6e570aacaad73aea12cb7
Aniket Chakraborty <achakraborty.webbies2025@gmail.com>
2025-10-15 15:11:20 +0530

    Add files via upload

--- userinput.py
+++ userinput.py
@@ -1,8 +1,7 @@
-# Test 1: Vaastu option, lighting-based direction
 user_inputs1 = {
     "space": "Bedroom",
     "mood": "Relaxed and Serene",
-    "lighting": "South-facing (morning sunlight)",
+    "lighting": "South facing room",
     "personality": "Dreamer",
     "vastu_choice": "Yes, strictly follow vaastu"
 }
@@ -10,8 +9,8 @@ user_inputs1 = {
 # Test 2: Vaastu option, explicit user direction Explicit direction
 user_inputs2 = {
     "space": "Living Room",
-    "mood": "Energetic and Vibrant",
-    "lighting": "East-facing (morning sunlight)",
+    "mood": "Bold and Dramatic",
+    "lighting": "East facing room",
     "personality": "Creative",
     "vastu_choice": "Yes, but flexible",
     "vastu_direction": "north east"  
@@ -19,9 +18,9 @@ user_inputs2 = {
 
 # Test 3: No Vaastu
 user_inputs3 = {
-    "space": "Study Room",
-    "mood": "Focused",
-    "lighting": "West-facing",
+    "space": "Balcony",
+    "mood": "Cozy and Inviting",
+    "lighting": "West facing room",
     "personality": "Analytical",
     "vastu_choice": "No"
 }
